ac6002e2e4b62905d7e09a891429248f4c921732
akachaisorn-del <akachai.sorn@gmail.com>
2025-08-09 17:53:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('hsk1-cache-v1').then(cache => cache.addAll(['/'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
+// Service worker placeholder
