3ff5e07f9aac201da9bc33284eb1b385491d3db6
akachaisorn-del <akachai.sorn@gmail.com>
2025-08-19 22:06:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('hsk1-v2').then(c=>c.addAll(['./'])))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('hsk3-radical-vanilla').then(c=>c.addAll(['./'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
